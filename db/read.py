67eca06bb42c63385092edf732e103bfbc4606c3
EstelleG10 <estelle.gerber@yale.edu>
2025-03-26 10:15:13 -0400

    added to read.py so we also great table for users w password

--- db/read.py
+++ db/read.py
@@ -22,6 +22,16 @@ cur.execute("""
         price NUMERIC NOT NULL
     );
 """)
+
+# make our users table 
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        username TEXT UNIQUE NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        password_hash TEXT NOT NULL
+    );
+""")
 conn.commit()
 
 # read our item csv 
