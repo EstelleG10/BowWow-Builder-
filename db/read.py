625b9b9393e39f8465034d882721bca768894abc
Amy Vataksi <amy@Amys-MacBook-Air.local>
2025-03-27 17:16:53 -0400

    “hi”

--- db/read.py
+++ db/read.py
@@ -6,7 +6,7 @@ import psycopg
 # i left user and pass blank for now bc we do not have users db 
 conn = psycopg.connect(
     dbname="itemsdb",
-    user="",     
+    user="postgres",     
     password="", 
     host="localhost",
     port="5432"
