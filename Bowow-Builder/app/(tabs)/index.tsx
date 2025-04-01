9f56fa3116ef7fc317384a3fc7e56cdb0a740041
Leslie714 <58919938+Leslie714@users.noreply.github.com>
2025-03-31 21:20:21 -0400

    update line ending to CRLF for windows compatibility

--- Bowow-Builder/app/(tabs)/index.tsx
+++ Bowow-Builder/app/(tabs)/index.tsx
@@ -1,12 +1,19 @@
-import { View, Text} from 'react-native'
+import { View, Text, StyleSheet} from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Home = () => {
     return (
-        <View>
-            <Text>Bow Wow Builder</Text>
-        </View>
+        <SafeAreaView>
+            <View>
+                <Text style = {styles.header}>Bow Wow Builder</Text>
+            </View>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+  header: {textAlign: 'left', fontSize: 48, fontWeight:'bold'},
+});
 
 export default Home;
