451a4f18dcf11290b5d5df7c4f096537ca394a08
amyv14 <144635611+amyv14@users.noreply.github.com>
2025-05-01 19:50:52 -0400

    Delete Bowow-Builder/db/etxt.py

--- Bowow-Builder/db/etxt.py
+++
@@ -1,9 +0,0 @@
-import psycopg
-
-conn = psycopg.connect("dbname=itemsdb user='' password='' host=localhost")
-cur = conn.cursor()
-cur.execute("SELECT * FROM items;")
-rows = cur.fetchall()
-print(rows)
-cur.close()
-conn.close()
